0ead2bcb34e515bcefc06f0cce7b044337aaa1a1
Enes <140523998+Wyltre@users.noreply.github.com>
2024-07-22 21:43:39 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+#| |     / /_  __/ / /_________ 
+#| | /| / / / / / / __/ ___/ _ \
+#| |/ |/ / /_/ / / /_/ /  /  __/
+#|__/|__/\__, /_/\__/_/   \___/ 
+#       /____/     
+
+token = "youraccounttokenhere"   # Token Here / Tokeni Buraya Girin.
+
+#github.com/wyltre
---
+++ online.py
@@ -0,0 +1,53 @@
+import config
+import json
+import time
+import websocket
+import requests
+
+status = "online"
+token = config.token
+headers = {"Authorization": token, "Content-Type": "application/json"}
+userinfo = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers).json()
+username = userinfo["username"]
+discriminator = userinfo["discriminator"]
+userid = userinfo["id"]
+#github.com/wyltre
+print("Başlatılıyor... ")
+time.sleep(2)  
+print("""Başlatılıyor... 
+| |   / /_ __/ / /_________ 
+| | /| / / / / / / __/ ___/ _ \
+| |/ |/ / /_/ / / /_/ / / __/
+|__/|__/\__, /_/\__/_/  \___/ 
+    /____/   """)
+
+time.sleep(2)  
+
+def keep_online(token, status):
+    ws = websocket.WebSocketApp("wss://gateway.discord.gg/?v=9&encoding=json",
+                                on_message = on_message,
+                                on_error = on_error,
+                                on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+
+def on_message(ws, message):
+    print(message)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("### Hesap Kapatıldı. ###")
+#github.com/wyltre
+def on_open(ws):
+    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 11","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
+    ws.send(json.dumps(auth))
+
+def run_keep_online():
+    print(f"Hesaba Başarılıyla Bağlanıldı {username}#{discriminator} ({userid}).")
+    while True:
+        keep_online(token, status)
+        time.sleep(30)
+
+run_keep_online()
